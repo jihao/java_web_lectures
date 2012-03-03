12ff661d0a0668cfc6940fe8b3b36bc3b13dbbd9
jihao <jacky.jihao@gmail.com>
2012-03-03 22:02:18 +0800

    demo servlet filter

---
+++ first-webapp/src/main/java/com/haojii/javaweb/first_webapp/filters/FilterB.java
@@ -0,0 +1,47 @@
+package com.haojii.javaweb.first_webapp.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class FilterB
+ */
+public class FilterB implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public FilterB() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println(this.getClass().getName() + ",IN,request="+request+",response"+response);
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+		System.out.println(this.getClass().getName() + ",OUT,request="+request+",response"+response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
